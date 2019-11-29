88efeeb82a2b1793e2b533493bf990c8f7ac342a
daniel.borges <daniel.borges@leucotron.com.br>
2019-11-29 16:28:25 -0300

    project restructuring

--- src/app/pages/entries/entries.module.ts
+++ src/app/pages/entries/entries.module.ts
@@ -1,6 +1,4 @@
 import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { ReactiveFormsModule } from '@angular/forms';
 
 import { CalendarModule } from "primeng/calendar";
 import { IMaskModule } from "angular-imask";
@@ -8,6 +6,7 @@ import { IMaskModule } from "angular-imask";
 import { EntriesRoutingModule } from './entries-routing.module';
 import { EntryListComponent } from './entry-list/entry-list.component';
 import { EntryFormComponent } from './entry-form/entry-form.component';
+import { SharedModule } from 'src/app/shared/shared.module';
 
 
 @NgModule({
@@ -16,9 +15,8 @@ import { EntryFormComponent } from './entry-form/entry-form.component';
     EntryFormComponent
   ],
   imports: [
-    CommonModule,
+    SharedModule,
     EntriesRoutingModule,
-    ReactiveFormsModule,
     IMaskModule,
     CalendarModule
   ]
